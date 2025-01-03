54cf77e9d3967988e68f0d05ede54c5ecd8b5e05
Sanskriti Shukla <138789759+Sanskriti65@users.noreply.github.com>
2025-01-03 22:18:57 +0530

    Update MainSection3.js

--- src/components/MainSection3.js
+++ src/components/MainSection3.js
@@ -10,16 +10,16 @@ function MainSection3() {
             <h1>Order our best food options</h1>
           </div>
           <div className="menu-dishes">
-            <div><a href=""><img src="/images/Burger.avif" alt="Burger" /></a></div>
-            <div><a href=""><img src="/images/Chinese.avif" alt="Chinese" /></a></div>
-            <div><a href=""><img src="/images/Ice Cream.avif" alt="Ice Cream" /></a></div>
-            <div><a href=""><img src="/images/Momo.avif" alt="Momo" /></a></div>
-            <div><a href=""><img src="/images/North Indian.avif" alt="North Indian" /></a></div>
-            <div><a href=""><img src="/images/Pastry.avif" alt="Pastry" /></a></div>
-            <div><a href=""><img src="/images/Pav Bhaji.avif" alt="Pav Bhaji" /></a></div>
-            <div><a href=""><img src="/images/Pizza.avif" alt="Pizza" /></a></div>
-            <div><a href=""><img src="/images/Pure Veg.avif" alt="Pure Veg" /></a></div>
-            <div><a href=""><img src="/images/Rolls.avif" alt="Rolls" /></a></div>
+            <div><a href="https://example.com"><img src="/images/Burger.avif" alt="Burger" /></a></div>
+            <div><a href="https://example.com"><img src="/images/Chinese.avif" alt="Chinese" /></a></div>
+            <div><a href="https://example.com"><img src="/images/Ice Cream.avif" alt="Ice Cream" /></a></div>
+            <div><a href="https://example.com"><img src="/images/Momo.avif" alt="Momo" /></a></div>
+            <div><a href="https://example.com"><img src="/images/North Indian.avif" alt="North Indian" /></a></div>
+            <div><a href="https://example.com"><img src="/images/Pastry.avif" alt="Pastry" /></a></div>
+            <div><a href="https://example.com"><img src="/images/Pav Bhaji.avif" alt="Pav Bhaji" /></a></div>
+            <div><a href="https://example.com"><img src="/images/Pizza.avif" alt="Pizza" /></a></div>
+            <div><a href="https://example.com"><img src="/images/Pure Veg.avif" alt="Pure Veg" /></a></div>
+            <div><a href="https://example.com"><img src="/images/Rolls.avif" alt="Rolls" /></a></div>
           </div>
         </div>
       </div>
