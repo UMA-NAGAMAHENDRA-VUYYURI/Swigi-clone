c15f507eae7fbcc88eeb7f395034fbaebbe3b393
Sanskriti Shukla <138789759+Sanskriti65@users.noreply.github.com>
2025-01-03 22:12:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 // src/App.js
 import React from 'react';
-import Header from './components/Header';  // Ensure this import is correct
+import Header from './components/Header'; // Ensure these paths are correct
 import MainSection1 from './components/MainSection1';
 import MainSection2 from './components/MainSection2';
 import MainSection3 from './components/MainSection3';
 import MainSection4 from './components/MainSection4';
 import MainSection5 from './components/MainSection5';
-<link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-/>
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap once in your entry point
 
 function App() {
   return (
-  
-
     <div>
       <Header />
       <MainSection1 />
@@ -23,7 +19,6 @@ function App() {
       <MainSection4 />
       <MainSection5 />
     </div>
-
   );
 }
 
